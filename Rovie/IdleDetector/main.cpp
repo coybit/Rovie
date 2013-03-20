8ed5049887676c4def587c4b3708745ebb47c1e9
coybit <coybit@gmail.com>
2013-03-20 20:53:27 +0430

    Idle Analyzer and Visualizer added

---
+++ Rovie/IdleDetector/main.cpp
@@ -0,0 +1,68 @@
+﻿#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+#include "sigar-bin/include/sigar.h"
+
+extern "C" {
+#include "sigar-bin/include/sigar_format.h"
+
+}
+
+#define SAMPLING_PERIOD 2000
+
+int main(int argc, char **argv) {
+    int status, i;
+    sigar_t *sigar;
+    sigar_cpu_list_t cpulist1, cpulist2;
+	sigar_cpu_perc_t perc;
+	bool isFirstTime = true;
+	FILE* idleDB;
+	unsigned long long sampleCount = 0;
+
+	// Initialization
+    sigar_open(&sigar);
+	idleDB = fopen( "C:\\idle.db", "wb" );
+
+	// Write number of CPUs in the file
+	status = sigar_cpu_list_get(sigar, &cpulist1);
+
+	if (status != SIGAR_OK) {
+		printf("cpu_list error: %d (%s)\n",
+				status, sigar_strerror(sigar, status));
+		return 1;
+	}
+
+	fwrite( &cpulist1.number, sizeof(int), 1, idleDB );
+	fclose(idleDB);
+
+	while( true ) 
+	{
+		cout << "\rSampling ... " << (++sampleCount);
+
+		sigar_cpu_list_get(sigar, &cpulist2);
+		
+		for (i=0; i<cpulist2.number; i++) {
+			sigar_cpu_perc_calculate(&cpulist1.data[i], &cpulist2.data[i], &perc);
+
+			BYTE idlePerc = 100*perc.idle;
+
+			idleDB = fopen( "C:\\idle.db", "ab+" );
+			fwrite( &idlePerc, sizeof(BYTE), 1, idleDB );
+			fclose(idleDB );
+		}
+
+		Sleep(SAMPLING_PERIOD);
+		
+		sigar_cpu_list_destroy(sigar, &cpulist1);
+		cpulist1 = cpulist2;
+	}
+
+	// Release resources
+	sigar_cpu_list_destroy(sigar, &cpulist2);
+    sigar_close(sigar);
+
+    return 0;
+}
---
+++ Rovie/IdleVisualizer/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace IdleVisualizer
+{
+    public partial class Form1 : Form
+    {
+        private List<byte> perc;
+        private Thread refreshThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            perc = new List<byte>();
+
+            refreshThread = new Thread(new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    this.Invoke(new Action(() => this.Refresh()));
+                    Thread.Sleep(2000);
+                }
+            }));
+            refreshThread.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            LoadData();
+
+            int x = 0;
+            int y;
+
+            foreach (char p in perc)
+            {
+                x++;
+                y = this.Height - p;
+                e.Graphics.FillRectangle(Brushes.Black, x, y, 1, y);
+            }
+        }
+        
+
+        private void LoadData()
+        {
+            try
+            {
+                BinaryReader db = new BinaryReader(File.Open("C:\\idle.db", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                int cpuNumber = db.ReadInt32();
+
+                perc.Clear();
+
+                while (db.BaseStream.Position < db.BaseStream.Length - 1)
+                {
+                    for (int i = 0; i < cpuNumber; i++)
+                    {
+                        byte idlePerc = db.ReadByte();
+
+                        if (i == 0)
+                            perc.Add(idlePerc);
+                    }
+                }
+            }
+            catch (Exception e) { }
+            //this.Refresh();
+            //Application.DoEvents();
+            //System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
